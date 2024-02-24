ebd08a65f3e352f45a463f16888c0677d2fc0700
ivanyshek <105326502+ivanyshek@users.noreply.github.com>
2024-02-24 19:30:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,47 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value > 1) {
+        for (int i = 2; i < value / 2 + 1; i++) {
+            if (value % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    } else {
+        return 0;
+    }
+    
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int count = 0;
+  uint64_t uPer = 2;
+    while(true){
+        if (checkPrime(uPer)) {
+            count++;
+        }
+        if (count == n) {
+            return uPer;
+        }
+        uPer++;
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    uint64_t uPer = value + 1;
+    while (!checkPrime(uPer)) {
+        uPer++;
+    }
+    return uPer;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t result = 0;
+    for (int i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            result += i;
+        }
+    }
+    return result;
 }
